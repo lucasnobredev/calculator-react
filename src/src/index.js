cc19f425d0b265f0812ec4d85c5d706800d9ee72
Lucas Nobre <lucasnobredev@outlook.com>
2019-02-17 04:40:37 -0300

    plus is working

--- src/src/index.js
+++ src/src/index.js
@@ -5,25 +5,87 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 class Calculator extends React.Component{
-    state = { firstAlgarismo:0, secondAlgarismo:0 }
+    state = { firstAlgarismo:0, secondAlgarismo:0, result:0, operator:"" }
     handleSubmit = (event) => {
         event.preventDefault();
         console.log('First:' + this.state.firstAlgarismo);
         console.log('Second:' + this.state.secondAlgarismo);
+        console.log('operator:' + this.state.operator);
+
+        this.setState({
+            result: parseInt(this.state.firstAlgarismo) + parseInt(this.state.secondAlgarismo)
+        });
+        
+        console.log('result:' + this.state.result);
     };
     render() {
         return (
-            <form onSubmit={this.handleSubmit}>
-            <input type="text"
-                value={this.state.firstAlgarismo}
-                onChange={(event) => this.setState({ firstAlgarismo: event.target.value})}
-                placeholder="first algarismo" />
-            <input type="text"
-                value={this.state.secondAlgarismo}
-                onChange={(event) => this.setState({ secondAlgarismo: event.target.value})}
-                placeholder="second algarismo" />
-            <button type="submit">Go</button>
+            <div className="col-3">
+                <form onSubmit={this.handleSubmit}>
+                    <div className="form-group">
+                        <label>First Number</label>
+                        <input type="text"
+                            className="form-control"
+                            value={this.state.firstAlgarismo}
+                            onChange={(event) => this.setState({ firstAlgarismo: event.target.value})}
+                            placeholder="first algarismo" />
+                    </div>
+                    <div className="form-group">
+                        <label>Choose operator:</label>
+                        <div className="row">
+                            <div className="col-3">
+                                <button style={{padding:0}} className="btn btn-info form-control" type="button" onClick={(event) => this.setState({ operator: "somar"})}>
+                                    <span style={{fontSize: 25 +'px', fontFamily: 'monospace'}}>
+                                    +
+                                    </span>
+                                </button>
+                            </div>
+                            <div className="col-3">
+                                <button style={{padding:0}} className="btn btn-info form-control" type="button" onClick={(event) => this.setState({ operator: "somar"})}>
+                                    <span style={{fontSize: 25 +'px', fontFamily: 'monospace'}}>
+                                    -
+                                    </span>
+                                </button>
+                            </div>
+                            <div className="col-3">
+                                <button style={{padding:0}} className="btn btn-info form-control" type="button" onClick={(event) => this.setState({ operator: "somar"})}>
+                                    <span style={{fontSize: 25 +'px', fontFamily: 'monospace'}}>
+                                    *
+                                    </span>
+                                </button>
+                            </div>
+                            <div className="col-3">
+                                <button style={{padding:0}} className="btn btn-info form-control" type="button" onClick={(event) => this.setState({ operator: "somar"})}>
+                                    <span style={{fontSize: 25 +'px', fontFamily: 'monospace'}}>
+                                    /
+                                    </span>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="form-group">
+                        <label>Second Number</label>
+                        <input type="text"
+                            className="form-control"
+                            value={this.state.secondAlgarismo}
+                            onChange={(event) => this.setState({ secondAlgarismo: event.target.value})}
+                            placeholder="second algarismo" />
+                    </div>
+
+                    <div className="form-group">
+                        <button className="btn btn-success form-control" type="submit">Calculate:</button>
+                    </div>
+                    <div className="form-group">
+                        <input type="text"
+                            disabled
+                            className="form-control"
+                            value={this.state.result}
+                             />
+                        </div>
+                    
                 </form>
+            </div>
         );
     }
 }
